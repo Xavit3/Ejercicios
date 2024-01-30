778818844751f03e8c82755bf3562e9700caf916
Xavit3 <117918647+Xavit3@users.noreply.github.com>
2024-01-30 18:32:57 -0300

    Update Figuras_Geometricas.java

--- Calcular_Area/src/Figuras_Geometricas.java
+++ Calcular_Area/src/Figuras_Geometricas.java
@@ -4,6 +4,8 @@ public class Figuras_Geometricas {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println ("Bienvenido");
+
+		System.out.printl ("Hola);
 	}
 
 }
